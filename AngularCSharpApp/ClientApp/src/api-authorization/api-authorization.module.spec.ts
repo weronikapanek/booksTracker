93c28712a8400ad925830f7d89addfeb0df0ec25
weronikapanek <werkap8811@gmail.com>
2022-08-21 09:15:39 +0200

    initial commit

---
+++ AngularCSharpApp/ClientApp/src/api-authorization/api-authorization.module.spec.ts
@@ -0,0 +1,13 @@
+﻿import { ApiAuthorizationModule } from './api-authorization.module';
+
+describe('ApiAuthorizationModule', () => {
+  let apiAuthorizationModule: ApiAuthorizationModule;
+
+  beforeEach(() => {
+    apiAuthorizationModule = new ApiAuthorizationModule();
+  });
+
+  it('should create an instance', () => {
+    expect(apiAuthorizationModule).toBeTruthy();
+  });
+});
---
+++ AngularCSharpApp/ClientApp/src/app/counter/counter.component.ts
@@ -0,0 +1,15 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-component',
+  templateUrl: './counter.component.html'
+})
+export class CounterComponent {
+  public currentCount = 0;
+  public text: string = '';
+
+  public incrementCounter() {
+    this.currentCount++;
+    this.currentCount % 2 === 0 ? this.text = 'I am countable' : this.text = 'I am uncountable';
+  }
+}
